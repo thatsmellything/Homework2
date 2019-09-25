d89c3eb1e7dae234cdb3795e611d97d237a73988
thatsmellything <thesmelliestman@gmail.com>
2019-09-25 12:13:32 -0600

    Update TestShape.java

--- src/tests/TestShape.java
+++ src/tests/TestShape.java
@@ -6,6 +6,9 @@ import model.Shape;
 
 public class TestShape {
 	
-	static ArrayList<Shape> shapeyBois = new ArrayList<Shape>();
+	static ArrayList<Shape> shapeyBois = new ArrayList<Shape>(8);
+	
+	shapeyBois.@add(new Triangle(50.9));
+
 
 }
