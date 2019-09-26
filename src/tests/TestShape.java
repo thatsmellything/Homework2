72051a4496813a95692a560c0c18688e704a04df
thatsmellything <thesmelliestman@gmail.com>
2019-09-26 17:34:37 -0600

    Update TestShape.java

--- src/tests/TestShape.java
+++ src/tests/TestShape.java
@@ -10,7 +10,13 @@ public class TestShape {
 	//create the child models
 	
 	Triangle triangleBaby = new Triagle(5.0);
+	Triangle triangleBaby = new Triagle(5.0);
 	Square squareChild = new Square(30.2);
+	Square squareChild = new Square(30.2);
+	Cube cubeBoi = new Cube(4.5);
+	Cube cubeBoi = new Cube(4.5);
+	Pyramid pLad = new Pyramid(9.9);
+	Pyramid pLad = new Pyramid(9.9);
 	
 	//Create the arraylist
 	
