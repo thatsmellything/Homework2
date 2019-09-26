1da9827966f886d16fcb8704ef1612e13e9f2eb3
thatsmellything <thesmelliestman@gmail.com>
2019-09-26 17:36:32 -0600

    Update TestShape.java

--- src/tests/TestShape.java
+++ src/tests/TestShape.java
@@ -10,22 +10,28 @@ public class TestShape {
 	//create the child models
 	
 	Triangle triangleBaby = new Triagle(5.0);
-	Triangle triangleBaby = new Triagle(5.0);
+	Triangle triangleBaby2 = new Triagle(5.0);
 	Square squareChild = new Square(30.2);
-	Square squareChild = new Square(30.2);
+	Square squareChild2 = new Square(30.2);
 	Cube cubeBoi = new Cube(4.5);
-	Cube cubeBoi = new Cube(4.5);
+	Cube cubeBoi2 = new Cube(4.5);
 	Pyramid pLad = new Pyramid(9.9);
-	Pyramid pLad = new Pyramid(9.9);
+	Pyramid pLad2 = new Pyramid(9.9);
 	
 	//Create the arraylist
 	
 	List<Shape> shapeyBois = new ArrayList<Shape>(8);
 	//Add the child models to the array
-	shapeyBois.add(squareChild);
-	
-	
 	shapeyBois.add(triangleBaby);
+	shapeyBois.add(triangleBaby2);
+	shapeyBois.add(squareChild);
+	shapeyBois.add(squareChild2);
+	shapeyBois.add(cubeBoi);
+	shapeyBois.add(cubeBoi2);
+	shapeyBois.add(pLad);
+	shapeyBois.add(pLad2);
+	
+	
 	
 	
 	//iterate through the array (hopefully)
