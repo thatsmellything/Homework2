d0c294a519c76bb5257a081cfae387911da30bb2
thatsmellything <thesmelliestman@gmail.com>
2019-09-27 13:49:05 -0600

    Update TestGUI.java

--- src/tests/TestGUI.java
+++ src/tests/TestGUI.java
@@ -2,12 +2,14 @@
 
  import javafx.application.Application;
  import javafx.collections.ObservableList;
- import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.Scene;
  import javafx.scene.control.TextField;
  import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
  
 public class TestGUI extends Application {
@@ -22,11 +24,31 @@ public class TestGUI extends Application {
 		//Creates the Pane for the GUI
 		StackPane pane = new StackPane();
 		
-		//Creates the text, changes the color and adds the text to the screen
-		Text helloText = new Text();
-		helloText.setText("Hello World");
-		helloText.setFill(Color.BLUE);
-		pane.getChildren().add(helloText);
+		
+		
+		//Creates the texts, changes the color and adds the text to the screen
+			//Hello portion
+			Text helloText = new Text();
+			helloText.setText("Hello ");
+			helloText.setFill(Color.BLUE);
+			//World portion
+			Text worldText = new Text();
+			worldText.setText("World!");
+			worldText.setFill(Color.DARKORANGE);
+			//Add the texts to the screen
+			
+			//Create a textflow so that the text doesnt overlap and make you sad
+			TextFlow TF = new TextFlow();
+			TF.setLayoutX(300);
+			TF.setLayoutY(200);
+			TF.getChildren().addAll(helloText,worldText);
+			
+
+			//create a group so you can center it in the screen
+			Group group = new Group(TF);
+		
+		//Add it to the pane and boom gottem	
+		pane.getChildren().addAll(group);
 		
 		//Sets the scene and the title 
 		//(which is the little thing that shows up at the top of applications)
